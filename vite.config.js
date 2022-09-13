d83fb74e220138ef3c60b1762f9a52cc2a4bff20
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-14 03:57:20 +0630

    fix: base url typo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/pinia-todo',
+    base: '/pinia-todo/',
     plugins: [vue({
         reactivityTransform: true
     })],
